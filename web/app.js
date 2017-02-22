ec095c51e70dc9829d14cff76acfa410ba07a5a0
sh.watanabe <sh.watanabe@iftc.co.jp>
2017-02-22 19:31:42 +0900

    add response message to line

--- web/app.js
+++ web/app.js
@@ -1,11 +1,100 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-var port = process.env.PORT || 3000;
+const http = require('http');
+const https = require('https');
+const crypto = require('crypto');
 
-// Serve static files
-// app.use(express.static(__dirname + '/static'));
+const HOST = 'api.line.me'; 
+const REPLY_PATH = '/v2/bot/message/reply';//リプライ用
+const CH_SECRET = process.env.CHANNEL_SECRET; //Channel Secretを指定
+const CH_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN; //Channel Access Tokenを指定
+const SIGNATURE = crypto.createHmac('sha256', CH_SECRET);
+const PORT = process.env.PORT || 3000;
 
-// Serve your app
-console.log('Served: http://localhost:' + port);
-app.listen(port);
+/**
+ * httpリクエスト部分
+ */
+const client = (replyToken, SendMessageObject) => {    
+    let postDataStr = JSON.stringify({ replyToken: replyToken, messages: SendMessageObject });
+    let options = {
+        host: HOST,
+        port: 443,
+        path: REPLY_PATH,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'X-Line-Signature': SIGNATURE,
+            'Authorization': `Bearer ${CH_ACCESS_TOKEN}`,
+            'Content-Length': Buffer.byteLength(postDataStr)
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        let req = https.request(options, (res) => {
+                    let body = '';
+                    res.setEncoding('utf8');
+                    res.on('data', (chunk) => {
+                        body += chunk;
+                    });
+                    res.on('end', () => {
+                        resolve(body);
+                    });
+        });
+
+        req.on('error', (e) => {
+            reject(e);
+        });
+        req.write(postDataStr);
+        req.end();
+    });
+};
+
+http.createServer((req, res) => {
+    if(req.url !== '/' || req.method !== 'POST'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('');
+    }
+
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk;
+    });        
+    req.on('end', () => {
+        if(body === ''){
+          console.log('body is empty');
+          return;
+        }
+
+        let WebhookEventObject = JSON.parse(body).events[0];
+        let SendMessageObject;
+        //メッセージが送られて来た場合
+        if(WebhookEventObject.type === 'message'){
+            if(WebhookEventObject.message.type === 'text'){
+                SendMessageObject = [{
+                    type: 'text',
+                    text: WebhookEventObject.message.text
+                }];
+            }
+            client(WebhookEventObject.replyToken, SendMessageObject)
+            .then((body)=>{
+                console.log(body);
+            },(e)=>{console.log(e)});
+        } else if(WebhookEventObject.type === 'beacon') {
+            SendMessageObject = [{
+                type: 'text',
+                text: 'beacon state is : ' + WebhookEventObject.beacon.type
+            }];
+            
+            client(WebhookEventObject.replyToken, SendMessageObject)
+            .then((body)=>{
+                console.log(body);
+            },(e)=>{console.log(e)});
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('su');
+    });
+
+}).listen(PORT);
+
+console.log(`Server running at ${PORT}`);
